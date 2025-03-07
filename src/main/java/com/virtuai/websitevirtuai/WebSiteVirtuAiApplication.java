1121ddba53ad36a441618934428436ef668b0957
YuriOnishi <yurifernandesonishi@gmail.com>
2025-03-07 16:25:31 -0300

    First Commit

---
+++ src/main/java/com/virtuai/websitevirtuai/WebSiteVirtuAiApplication.java
@@ -0,0 +1,13 @@
+package com.virtuai.websitevirtuai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSiteVirtuAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSiteVirtuAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/virtuai/websitevirtuai/WebSiteVirtuAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtuai.websitevirtuai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSiteVirtuAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
